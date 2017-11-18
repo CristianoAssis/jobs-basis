bbccaa79fb799b925697bbf9ce933e20ffa3e94d
CristianoAssis <33484892+CristianoAssis@users.noreply.github.com>
2017-11-18 21:04:39 -0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Basis - Vagas</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <header>
+      
+    </header>
+
+    <div class="container">
+
+      <div class="row">
+        
+        <div class="col-md-3">
+          <img src="img/logo.png" alt="Logo" />
+        </div>
+
+        <div class="col-md-9 text-right">
+          <h3>Divulgação de Vagas</h3>
+        </div>
+
+        <div class="col-md-12">
+          <hr>
+        </div>
+
+      </div>
+
+      <div class="row">
+
+        <div class="col-md-4">
+          <div>
+            <a href="pcd.php">
+              <img src="img/icoPcd.png" alt="Ico" />
+              <h5>
+                Vagas para PCD
+              </h5>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div>
+            <a href="single.php">
+              <img src="img/icoJob.png" alt="Ico" />
+              <h5>
+                Vaga única
+              </h5>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div>
+            <a href="mult.php">
+              <img src="img/icoJobs.png" alt="Ico" />
+              <h5>
+                Vagas Diversas
+              </h5>
+            </a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <footer></footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ mult.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>Vaga Basis</title>
+      <meta charset="UTF-8">
+
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="css/global.css">
+  </head>
+
+  <body class="mult">
+    <div id="content-export">
+
+      <header>
+        <img src="img/mult/headerMult.png" />
+      </header>
+
+      <div class="text-content">
+      
+        <div class="content-gray">
+            <ul>
+              <li>
+                <b><input type="text" value="Cargo Aqui"> </b>
+                <textarea onkeyup="auto_grow(this)">digite a formação aqui...</textarea>
+
+                <b><input type="text" value="Cargo Aqui"> </b>
+                <textarea onkeyup="auto_grow(this)">digite a formação aqui...</textarea>
+              </li>
+
+              <li>
+                <b><input type="text" value="Cargo Aqui"> </b>
+                <textarea onkeyup="auto_grow(this)">digite a formação aqui...</textarea>
+
+                 <b><input type="text" value="Cargo Aqui"> </b>
+                <textarea onkeyup="auto_grow(this)">digite a formação aqui...</textarea>
+              </li>
+
+            </ul>
+            
+        </div>
+
+        <img src="img/mult/footer.png" alt="Footer Image" />
+
+      </div>
+
+    </div><!-- / content-export -->
+         
+    <div class="btn-download">
+      <a id="generatorImg">Clique aqui para finalizar</a> 
+      <a id="downloadImg" href="" style="display:none;" download>Fazer Download</a>
+
+      <a class="voltar" href="index.html">Voltar</a>
+    </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <script src="js/html2canvas.js"></script>
+      
+      <script>
+         $(function(){
+
+          $("#generatorImg").click(function(){
+
+            html2canvas($("#content-export"), {
+              onrendered: function(canvas) {
+
+                var imgsrc = canvas.toDataURL("image/png");
+                console.log(imgsrc);
+                $("#downloadImg").attr('href',imgsrc);
+                $("#generatorImg").hide();
+                $("#downloadImg").show();
+
+                $('#downloadImg').click(function() {
+                    location.reload();
+                });
+
+              }
+
+            });
+
+          }); 
+
+          $('#combo').change(function(){
+            if($(this).val() == 'java'){
+              $('#change-url').attr('src','img/single/java.png');
+            }
+            if($(this).val() == 'gerente'){
+              $('#change-url').attr('src','img/single/gerente.png');
+            }
+            if($(this).val() == 'desenvolvimento'){
+              $('#change-url').attr('src','img/single/desenvolvimento.png');
+            }
+            if($(this).val() == 'estagio'){
+              $('#change-url').attr('src','img/single/stag.png');
+            }
+          });
+
+        });
+
+        // Auto Size Textarea Campo
+        function auto_grow(element) {
+            element.style.height = "15px";
+            element.style.height = (element.scrollHeight)+"px";
+        }
+      </script>
+
+   </body>
+</html>
